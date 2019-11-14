5c90529f439b9a2d2d2026b65030c09d6a6eab16
Christiam Hernandez <christiamhernandez@Administrators-MacBook-Pro.local>
2019-11-14 16:42:12 -0500

    Commenting out server

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const studentSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    age: { type: Number, required: true },
-    schoolGrade: { type: Number, required: true },
-    gpa: { type: Number, required: true },
-    date: { type: Date, required: true, default: Date.now },
-    image: { type: String, required: true }
-});
+// const studentSchema = new Schema({
+//     firstName: { type: String, required: true },
+//     lastName: { type: String, required: true },
+//     age: { type: Number, required: true },
+//     schoolGrade: { type: Number, required: true },
+//     gpa: { type: Number, required: true },
+//     date: { type: Date, required: true, default: Date.now },
+//     image: { type: String, required: true }
+// });
 
-const Student = mongoose.model('Student', studentSchema);
+// const Student = mongoose.model('Student', studentSchema);
 
-module.exports = Student;
+// module.exports = Student;
